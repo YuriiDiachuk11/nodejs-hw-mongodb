86995b6067a5340fe8b24463e1e9d3b49b0a0df1
YD <yuriidiachuk@icloud.com>
2025-01-16 14:33:17 +0200

    setup MongoDB

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+bootstrap();
