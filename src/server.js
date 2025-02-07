1dde9ca30ea1de38fa5439e5cd019ffe5316d2c8
YD <yuriidiachuk@icloud.com>
2025-02-07 21:25:33 +0200

    add logs

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
+console.log('authRouter loaded');
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
 
 dotenv.config();
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 3000;
 
 export const setupServer = () => {
   const app = express();
@@ -25,6 +26,14 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
+  app.use((req, res, next) => {
+    console.log('Request Method:', req.method, 'Request URL:', req.url);
+    next();
+  });
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+  });
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
